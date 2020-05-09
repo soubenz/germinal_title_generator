28bdd75bd43e5ecbf37b83b210a27ca507aae065
Benzaoui <benzaoui.soufiane@dataimpact.fr>
2020-05-10 01:21:05 +0200

    Change button title

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -51,7 +51,7 @@
           </v-card-text>-->
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="green darken-1" text @click="dialog = false">Close</v-btn>
+            <v-btn color="green darken-1" text @click="dialog = false">Fermer</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
